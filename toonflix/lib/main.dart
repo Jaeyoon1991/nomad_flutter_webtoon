54318b6302573c484cb52a8a17075aa6acbd754b
jaeyoon lee <rainbowcrayon91@gmail.com>
2023-03-17 21:19:00 +0900

    UI Challenge 3.0 Header

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -32,15 +32,39 @@ class App extends StatelessWidget {
     // return CupertinoApp;
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Flutter, I'm testing!"),
-          centerTitle: false,
-          elevation: 10,
-        ),
-        body: Center(
-          child: Text("Hello World, I'm here again"),
-        ),
-      ),
+          backgroundColor: Color(0xff181818),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Hey, Selena',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w800),
+                        ),
+                        Text('Welcom Back',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 18,
+                            ))
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
